67e16298030d9b72771fb7e89b89430c6bd5084c
Rafael-Azvedo <112179732+Azvedo@users.noreply.github.com>
2023-08-10 08:38:49 -0300

    Update(XOR,OR,SUB) alu.sv

--- design/alu.sv
+++ design/alu.sv
@@ -11,14 +11,25 @@ module alu#(
         input logic [OPCODE_LENGTH-1:0]    Operation,
         output logic[DATA_WIDTH-1:0] ALUResult
         );
-    
+        
+        reg [DATA_WIDTH-1:0] immediate;
+        assign immediate = $signed(SrcB[31:20]);
+        
         always_comb
         begin
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+            4'b0001:        // OR
+                    ALUResult = SrcA | SrcB; 
+            4'b0101:        // XOR
+                    ALUResult = SrcA ^ SrcB; 
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
+            4'b0110:        // SUB
+                    ALUResult = SrcA - SrcB;
+            4'b1100:	    // ADDI
+                    ALUResult = SrcA + immediate;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
             default:
