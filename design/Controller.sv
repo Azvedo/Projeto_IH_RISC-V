a9f4b96bd777a58dd4f5e4e0e00755471f0b2bb2
Rafael-Azvedo <112179732+Azvedo@users.noreply.github.com>
2023-08-15 10:55:21 -0300

    Update Controller(ADDI).sv

--- design/Controller.sv
+++ design/Controller.sv
@@ -19,16 +19,17 @@ module Controller (
     output logic Branch  //0: branch is not taken; 1: branch is taken
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR;
+  logic [6:0] R_TYPE, LW, SW, BR, ADDI;
 
   assign R_TYPE = 7'b0110011;  //add,and
+  assign ADDI = 7'b0010011; //ADDI
   assign LW = 7'b0000011;  //lw
   assign SW = 7'b0100011;  //sw
   assign BR = 7'b1100011;  //beq
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == ADDI);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == ADDI);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR);
